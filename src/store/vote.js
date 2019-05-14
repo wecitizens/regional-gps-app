873a6517511bdf85ec3babe509cf34aef4a88cc8
jm768van <thelys-768@axonium.eu>
2019-05-14 18:13:37 +0200

    extend district data

--- src/store/vote.js
+++ src/store/vote.js
@@ -6,11 +6,20 @@ export default {
     current: {
       region:null,
       district: null,
+      regDistrict: null,
+      fedDistrict: null,
+      eurDistrict: null,
       election: null,
       surveyKey: null
     },
     districts: [],
-    districtSearchResults: []
+    eurDistricts: [],
+    fedDistricts: [],
+    regDistricts: [],
+    districtSearchResults: [],
+    eurDistrictSearchResults: [],
+    fedDistrictSearchResults: [],
+    regDistrictSearchResults: []
   },
   mutations: {
     setCurrentRegion(state, payload) {
@@ -26,6 +35,18 @@ export default {
       //console.log('vote.js:mut.setCurrentDistrict', payload);
       state.current.district = payload
     },
+    setCurrentRegDistrict (state, payload) {
+      //console.log('vote.js:mut.setCurrentDistrict', payload);
+      state.current.regDistrict = payload
+    },
+    setCurrentEurDistrict (state, payload) {
+      //console.log('vote.js:mut.setCurrentDistrict', payload);
+      state.current.eurDistrict = payload
+    },
+    setCurrentFedDistrict (state, payload) {
+      //console.log('vote.js:mut.setCurrentDistrict', payload);
+      state.current.fedDistrict = payload
+    },
     setCurrentElection (state, payload) {
       //console.log('vote.js:mut.setCurrentElection:', payload);
       state.current.election = payload
@@ -34,8 +55,29 @@ export default {
       //console.log('vote.js:mut.setDistricts:', payload);
       state.districts = payload
     },
+    setEurDistricts (state, payload) {
+      console.log('vote.js:mut.setEurDistricts:', payload);
+      state.eurDistricts = payload
+    },
+    setRegDistricts (state, payload) {
+      //console.log('vote.js:mut.setRegDistricts:', payload);
+      state.regDistricts = payload
+    },
+    setFedDistricts (state, payload) {
+      //console.log('vote.js:mut.setFedDistricts:', payload);
+      state.fedDistricts = payload
+    },
     setDistrictSearchResults (state, payload) {
       state.districtSearchResults = payload
+    },
+    setEurDistrictSearchResults (state, payload) {
+      state.eurDistrictSearchResults = payload
+    },
+    setFedDistrictSearchResults (state, payload) {
+      state.fedDistrictSearchResults = payload
+    },
+    setRegDistrictSearchResults (state, payload) {
+      state.regDistrictSearchResults = payload
     }
   },
   actions: {
@@ -51,6 +93,46 @@ export default {
 
       commit('setDistricts', districts)
     },
+    async getEurElectoralDistricts({ commit }, data) {
+      console.log('vote.js:act.getEurElectoralDistricts:');
+      const elEurDistricts= await API.get('vote/electoral-districts.json?reg=EUROP', data)
+          .then((request) => {
+            console.log(request.data);
+            Vue.i18n.add('en', {vote: request.data.i18n.en})
+            Vue.i18n.add('fr', {vote: request.data.i18n.fr})
+            Vue.i18n.add('nl', {vote: request.data.i18n.nl});
+            //console.log(request.data.data);
+            return request.data.data
+          })
+      commit('setEurDistricts', elEurDistricts)
+    },
+    async getRegElectoralDistricts({ commit }, data) {
+      console.log('vote.js:act.getRegElectoralDistricts:');
+      const elRegDistricts= await API.get('vote/electoral-districts.json?reg=BER', data)
+          .then((request) => {
+            //console.log(request.data);
+            Vue.i18n.add('en', {vote: request.data.i18n.en})
+            Vue.i18n.add('fr', {vote: request.data.i18n.fr})
+            Vue.i18n.add('nl', {vote: request.data.i18n.nl});
+            //console.log(request.data.data);
+            return request.data.data
+          })
+      commit('setRegDistricts', elRegDistricts)
+    },
+    async getFedElectoralDistricts({ commit }, data) {
+      console.log('vote.js:act.getFedElectoralDistricts:');
+      const elDistricts= await API.get('vote/electoral-districts.json?reg=BEFCH', data)
+          .then((request) => {
+            //console.log(request.data);
+            Vue.i18n.add('en', {vote: request.data.i18n.en})
+            Vue.i18n.add('fr', {vote: request.data.i18n.fr})
+            Vue.i18n.add('nl', {vote: request.data.i18n.nl});
+            //console.log(request.data.data);
+            return request.data.data
+          })
+      commit('setFedDistricts', elDistricts)
+    },
+
     async getElectoralDistricts({ commit }, data) {
       //console.log('vote.js:act.getElectoralDistricts:');
       const elDistricts= await API.get('vote/electoral-districts.json', data)
@@ -79,6 +161,48 @@ export default {
       const results = queryString ? districts.filter(createDistrictFilter(queryString)) : districts
       commit('setDistrictSearchResults', results)
     },
+    filterEurDistricts ({commit, state}, queryString) {
+      console.log('vote.js:act.filterEurDistricts ');
+      const eurDistricts = state.eurDistricts.map(m => {
+        m.value = m.code + ' ' + Vue.i18n.translate('vote.' + m.name)
+        return m
+      })
+      const createEurDistrictFilter = function (queryString) {
+        return (eurDistrict) => {
+          return (eurDistrict.value.toLowerCase().includes(queryString.toLowerCase()))
+        }
+      }
+      const results = queryString ? eurDistricts.filter(createEurDistrictFilter(queryString)) : eurDistricts
+      commit('setEurDistrictSearchResults', results)
+    },
+    filterRegDistricts ({commit, state}, queryString) {
+      console.log('vote.js:act.filterRegDistricts ');
+      const regDistricts = state.regDistricts.map(m => {
+        m.value = m.code + ' ' + Vue.i18n.translate('vote.' + m.name)
+        return m
+      })
+      const createRegDistrictFilter = function (queryString) {
+        return (regDistrict) => {
+          return (regDistrict.value.toLowerCase().includes(queryString.toLowerCase()))
+        }
+      }
+      const results = queryString ? regDistricts.filter(createRegDistrictFilter(queryString)) : regDistricts
+      commit('setRegDistrictSearchResults', results)
+    },
+    filterFedDistricts ({commit, state}, queryString) {
+      console.log('vote.js:act.filterFedDistricts ');
+      const fedDistricts = state.fedDistricts.map(m => {
+        m.value = m.code + ' ' + Vue.i18n.translate('vote.' + m.name)
+        return m
+      })
+      const createFedDistrictFilter = function (queryString) {
+        return (fedDistrict) => {
+          return (fedDistrict.value.toLowerCase().includes(queryString.toLowerCase()))
+        }
+      }
+      const results = queryString ? fedDistricts.filter(createFedDistrictFilter(queryString)) : fedDistricts
+      commit('setFedDistrictSearchResults', results)
+    },
     async setCurrentMunicipalElection ({commit}, district) {
       if(district) {
         const election = await API.get('vote/election/2019_be_regional/district/' + district.key + '.json').then((request) => {
@@ -93,8 +217,7 @@ export default {
         console.log('District undefined !');
       }
     },
-    async setCurrentElection ({commit}, district) {
-      if(district) {
+    async setCurrentElection ({commit}, someData) {
         const election = await API.get('vote/election/2019_be_reg_fed_eur.json').then((request) => {
           //console.log('vote.js:act.setCurrentElection'); console.log(request.data.i18n);
           Vue.i18n.add('en', {vote: request.data.i18n.en})
@@ -104,9 +227,6 @@ export default {
         })
         commit('setCurrentElection', election)
         commit('setCurrentSurveyKey', '2019-5-26')
-      } else {
-        console.log('District undefined !');
-      }
     }
   },
   getters: {
