2a8672c0ed8dc2a7f09e8580eb48259123cf63da
Daniel Sum <daniel@lacerisenumerique.be>
2019-05-15 01:17:32 +0200

    - rebuild dist for demo

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -11,10 +11,10 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b32b64f4811f0928ba72b4279db66bf1.js"
+  "/precache-manifest.fbff4a78a77b0aed65f42e651aa52df0.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "gps"});
